75f6ba5f5594ebca9593422cab7d7d5398aeab3b
Jakub Mamelski <jakub.mamelski@outlook.com>
2020-10-30 12:19:52 +0100

    sync addition

--- Async-await/async-await.js
+++ Async-await/async-await.js
@@ -7,5 +7,14 @@ function increaseSalary(base, increase) {
     return newSalary;
 }
 
+// Sync addition
+function slowAddition(n1,n2){
+    return new Promise(resolve =>{
+        setTimeout(()=>resolve(n1 + n2), 2000);
+    })
+}
+
 increaseSalary(1000, 200); // => 1200
 // logs "New salary: 1200"
+
+slowAddition(1,7).then(sum => console.log('suma: ' + sum));
